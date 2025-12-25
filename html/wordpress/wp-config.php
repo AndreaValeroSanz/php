1d8a0f1a741318f6367bffd3661a6b479552e4a9
andrea <Andrea.valero.sanz@gmail.com>
2025-12-25 22:13:50 +0100

    testfile

--- html/wordpress/wp-config.php
+++ html/wordpress/wp-config.php
@@ -20,16 +20,10 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'mysql' );
-
-/** Database username */
-define( 'DB_USER', 'root' );
-
-/** Database password */
-define( 'DB_PASSWORD', '123' );
-
-/** Database hostname */
-define( 'DB_HOST', 'db' );
+define( 'DB_NAME', 'wordpress1' );
+define( 'DB_USER', 'wordpress1' );
+define( 'DB_PASSWORD', 'DWD8Ds3l4dvXpjZH' );
+define( 'DB_HOST', 'localhost' ); 
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
